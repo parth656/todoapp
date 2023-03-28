e0cf38628941819cd46b0a9ff890d96eeedeba8f
parth656 <parthbijpuriya416@gmail.com>
2023-03-28 12:49:02 +0530

    deploy

---
+++ pages/index.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [completedTodos, setCompletedTodos] = useState([]);
+
+  const emojis = ["😀", "😆", "😊", "😍", "🤩", "🥳"];
+
+  const handleAddTodo = () => {
+    if (newTodo.trim().length === 0) return;
+
+    const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    setTodos([...todos, { id: Date.now(), text: newTodo, emoji: randomEmoji }]);
+    setNewTodo("");
+  };
+
+  const handleDeleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+    setCompletedTodos(completedTodos.filter((todo) => todo.id !== id));
+  };
+
+ 
+const handleCompleteTodo = (id) => {
+  const todo = todos.find((todo) => todo.id === id);
+  setCompletedTodos([...completedTodos, { ...todo, completed: true }]);
+  setTodos(todos.filter((todo) => todo.id !== id));
+};
+
+  const handleRemoveCompletedTodos = () => {
+    setCompletedTodos([]);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        placeholder="Enter a new todo..."
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              id={`todo-${todo.id}`}
+              onChange={() => handleCompleteTodo(todo.id)}
+            />
+            <label htmlFor={`todo-${todo.id}`}>{todo.emoji} {todo.text}</label>
+            <button onClick={() => handleDeleteTodo(todo.id)}>❌</button>
+          </li>
+        ))}
+      </ul>
+      <div className="completed-todos">
+        <h2>Completed Todos</h2>
+        {completedTodos.length > 0 ? (
+          <ul>
+            {completedTodos.map((todo) => (
+              <li key={todo.id}>
+                <input
+                  type="checkbox"
+                  id={`completed-${todo.id}`}
+                  checked
+                  disabled
+                />
+                <label htmlFor={`completed-${todo.id}`} className="completed">
+                  {todo.emoji} {todo.text}
+                </label>
+                <button onClick={() => handleDeleteTodo(todo.id)}>❌</button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No completed todos yet.</p>
+        )}
+        {completedTodos.length > 0 && (
+          <button onClick={handleRemoveCompletedTodos}>Clear Completed</button>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ pages/todo.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!newTodo.trim()) return;
+    setTodos([...todos, newTodo.trim()]);
+    setNewTodo('');
+  };
+
+  const handleDelete = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(event) => setNewTodo(event.target.value)}
+          placeholder="Add new todo"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
